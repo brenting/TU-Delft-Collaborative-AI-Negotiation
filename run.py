a17a0dead93c7ab4424be69e6c36c4c9768b53f7
Thijs Nulle <t.j.nulle@student.tudelft.nl>
2022-02-17 14:52:46 +0100

    Agent version 2

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 from utils.plot_trace import plot_trace
 from utils.runners import run_session
@@ -12,13 +13,23 @@ if not os.path.exists("results"):
 #   We need to specify the classpath of 2 agents to start a negotiation.
 #   We need to specify the preference profiles for both agents. The first profile will be assigned to the first agent.
 #   We need to specify a deadline of amount of rounds we can negotiate before we end without agreement
+
+rand_x = random.randint(0,9)
+rand_agent = random.choice([
+    "agents.agents.agent_one.AgentOne",
+    "agents.boulware_agent.boulware_agent.BoulwareAgent",
+    "agents.template_agent.template_agent.TemplateAgent",
+    "agents.conceder_agent.conceder_agent.ConcederAgent",
+    "agents.hardliner_agent.hardliner_agent.HardlinerAgent"
+])
+
 settings = {
     "agents": [
-        "agents.random_agent.random_agent.RandomAgent",
-        "agents.template_agent.template_agent.TemplateAgent",
+        "agents.agents.agent_one.AgentOne",
+        rand_agent
     ],
-    "profiles": ["domains/domain00/profileA.json", "domains/domain00/profileB.json"],
-    "deadline_rounds": 200,
+    "profiles": [f"domains/domain0{rand_x}/profileA.json", f"domains/domain0{rand_x}/profileB.json"],
+    "deadline_rounds": 200
 }
 
 # run a session and obtain results in dictionaries
