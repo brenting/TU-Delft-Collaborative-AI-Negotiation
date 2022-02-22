348655bb0b40556370c1c7f3ad3489e15b172bd7
Thijs Nulle <t.j.nulle@student.tudelft.nl>
2022-02-22 10:22:10 +0100

    Finalise agent 4

--- agents/agents/agent_four.py
+++ agents/agents/agent_four.py
@@ -2,7 +2,9 @@ from agents.template_agent.template_agent import TemplateAgent
 
 import random, time
 from collections import Counter
+from typing import cast
 
+from geniusweb.opponentmodel.FrequencyOpponentModel import FrequencyOpponentModel
 from geniusweb.actions.Accept import Accept
 from geniusweb.actions.Offer import Offer
 from geniusweb.bidspace.AllBidsList import AllBidsList
@@ -22,7 +24,7 @@ class AgentFour(TemplateAgent):
     def __init__(self):
         super().__init__()
 
-        self.best_bid = (None, 0.0)
+        self.best_bid = (None, float('-inf'))
         self.last_bid = None
         self.utility_value = 0.9
         self.reservation_value = 0.6
@@ -31,55 +33,66 @@ class AgentFour(TemplateAgent):
         super().notifyChange(info)
 
         if isinstance(info, Settings):
-            # Create a counter for all the values per issue of the opponent.
-            self.counter = dict((k,Counter()) for k in self._profile.getProfile().getUtilities().keys())
-
             # Create a list of all bids and sort them based on their utility function value.
             self.all_bids = [ (bid, self._profile.getProfile().getUtility(bid)) for bid in AllBidsList(self._profile.getProfile().getDomain()) ]
             self.all_bids = [ x for x in self.all_bids if x[1] > self.reservation_value ]
             self.all_bids.sort(key=lambda x:x[1], reverse=True)
 
+            # Create the frequency opponent model.
+            self.opponent_model = FrequencyOpponentModel.create().With(self._profile.getProfile().getDomain(), None)
+
         elif isinstance(info, ActionDone):
-            self.last_bid = self._last_received_bid
-            self.curr_round = self._progress.getCurrentRound()
+            action: Action = cast(ActionDone, info).getAction()
+
+            if isinstance(action, Offer):
+                # Update the last bid and the opponent model.
+                self.last_bid = cast(Offer, action).getBid()
+                self.opponent_model = self.opponent_model.WithAction(action, self._progress)
+
+                # Lower our reservation value over time, not allowing opponents to use it against us.
+                self.reservation_value = 0.9 - (1 - self.progress())**2 * 0.3
 
     def getDescription(self) -> str:
         return "Agent 4"
 
     def _myTurn(self):
+        # Determine what action we need to propose.
         action = Accept(self._me, self.last_bid) if self._isGood(self.last_bid) else Offer(self._me, self._findBid())
         self.getConnection().send(action)
 
     def _isGood(self, bid: Bid):
-        if bid is None:
-            return False
-
-        for issue in bid.getIssues():
-            self.counter[issue][bid.getValue(issue)] += 1
-
-        return self._profile.getProfile().getUtility(bid) > self.utility_value
-
+        return bid and self._profile.getProfile().getUtility(bid) > self.utility_value
 
     def _findBid(self):
+        # If there is no previous bid, return our best bid.
         if self.last_bid is None:
             return self.all_bids.pop(0)[0]
 
-        # Generate all the good bids.
+        if self.progress() > 0.95:
+            return self.best_bid[0]
+
+        # Update our best received bid.
+        if self.best_bid[1] < self._profile.getProfile().getUtility(self.last_bid):
+            self.best_bid = (self.last_bid, self._profile.getProfile().getUtility(self.last_bid))
+
+        # Generate all the bids with a high enough utility value.
         good_bids = [ b for b in self.all_bids if b[1] > self.utility_value ]
+
+        # If no good enough bids exist, lower our utility value and return our best bid.
         if not good_bids:
-            self.utility_value = max(self.utility_value - 0.01, self.reservation_value)
-            return self.all_bids.pop(0)[0]
+            self.utility_value = max(self.utility_value - 0.02, self.reservation_value)
+            return self.all_bids.pop(0)[0] if len(self.all_bids) > 1 else self.all_bids[0][0]
 
-        # Calculate similarities to the proposed bids of the opponent.
-        similarities = [ sum([ self.counter[i][bid.getValue(i)] / (self._progress.getCurrentRound() + 1) for i in bid.getIssues() ]) / len(bid.getIssues()) for bid, u in good_bids ]
-        ix = sorted(range(len(similarities)), key=lambda k: similarities[k], reverse=True)[:10]
+        # Calculate similarities to the proposed bids of the opponent based on the opponent model.
+        similarities = [ self.opponent_model.getUtility(bid) for bid, u in good_bids ]
+        index = similarities.index(max(similarities))
 
-        # If none of the bids are similar enough, update the utility value.
-        if similarities[ix[0]] < (self._progress.getCurrentRound() / self._progress.getTotalRounds()) ** 3 and \
-           (self._progress.getCurrentRound() / self._progress.getTotalRounds()) < 0.25:
-            self.utility_value = max(self.utility_value - 0.01, self.reservation_value)
-
-        bid = sorted([ good_bids[i] for i in ix ], key=lambda x:x[1], reverse=True)[0]
+        # If none of the bids are similar enough, lower our utility value.
+        if similarities[index] < 0.5 and self.progress() > 0.5:
+            self.utility_value = max(self.utility_value - 0.005, self.reservation_value)
 
         # Remove the bid to be placed from the list and return it.
-        return self.all_bids.pop(self.all_bids.index(bid))[0]
+        return self.all_bids.pop(self.all_bids.index(good_bids[index]))[0]
+
+    def progress(self):
+        return self._progress.getCurrentRound() / self._progress.getTotalRounds()
